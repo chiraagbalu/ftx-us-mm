8204aa871638d55060abf75653c1b94ac44048ef
Chiraag Balu <chiraagbalu@berkeley.edu>
2022-10-01 17:34:57 -0700

    add

--- serialmm.py
+++ serialmm.py
@@ -47,6 +47,7 @@ init_inventory = bals[['coin', 'usdValue']].set_index('coin').to_dict()[
 inventory = init_inventory
 # calculate total value of account, and amount of money per market
 usd_total = rest.get_total_usd_balance()
+init_usd = usd_total
 usd_per = usd_total/(2*len(markets)) * (1-0.0002)
 coins = [market[:-4] for market in markets]
 
@@ -63,14 +64,16 @@ for coin in coins:
             rest.place_order(market=f'{coin}/USD', side='sell',
                              size=delta/sellp*1.0001, price=sellp)
         except Exception as error:
-            print(f'{error}')
+            pass
+            # print(f'{error}')
     elif delta < 0:
         buyp = sock.get_orderbook(f'{coin}/USD')['asks'][0][0]
         try:
             rest.place_order(market=f'{coin}/USD', side='buy',
                              size=-delta/buyp*1.0001, price=buyp)
         except Exception as error:
-            print(f'{error}')
+            pass
+            # print(f'{error}')
     marketp = max(sellp, buyp)
     # initial quote
     rest.place_order(f'{coin}/USD', side='sell', price=marketp*1.19,
@@ -112,39 +115,41 @@ while True:
         sell_quote = cur_quote[cur_quote['side'] == 'sell']
 
         if not buy_quote.empty:
-            print(f'{market} bids: {len(buy_quote)}')
+            #print(f'{market} bids: {len(buy_quote)}')
 
             if len(buy_quote) > maxquotes:
-                print(f'canceling: {len(buy_quote) - maxquotes}')
+                #print(f'canceling: {len(buy_quote) - maxquotes}')
                 cancel_buys = buy_quote.sort_values(by='price')[:-maxquotes]
                 cancel_ids = list(cancel_buys['id'])
                 for id in cancel_ids:
                     try:
                         rest.cancel_order(id)
                     except Exception as error:
-                        print(error)
+                        pass
+                        # print(error)
                 buy_quote = buy_quote.sort_values(by='price')[-maxquotes:]
 
-            print(f'{market} bids: {len(buy_quote)}')
+            #print(f'{market} bids: {len(buy_quote)}')
             cur_bid_id = buy_quote['id'].item()
             cur_bid = buy_quote['price'].item()
             nobid = False
 
         if not sell_quote.empty:
-            print(f'{market} asks: {len(sell_quote)}')
+            #print(f'{market} asks: {len(sell_quote)}')
 
             if len(sell_quote) > maxquotes:
-                print(f'canceling: {len(sell_quote) - maxquotes}')
+                #print(f'canceling: {len(sell_quote) - maxquotes}')
                 cancel_sells = sell_quote.sort_values(by='price')[:-maxquotes]
                 cancel_ids = list(cancel_sells['id'])
                 for id in cancel_ids:
                     try:
                         rest.cancel_order(id)
                     except Exception as error:
-                        print(error)
+                        pass
+                        # print(error)
                 sell_quote = sell_quote.sort_values(by='price')[-maxquotes:]
 
-            print(f'{market} asks: {len(sell_quote)}')
+            #print(f'{market} asks: {len(sell_quote)}')
             cur_ask_id = sell_quote['id'].item()
             cur_ask = sell_quote['price'].item()
             noask = False
@@ -174,13 +179,15 @@ while True:
                 rest.place_order(market, side='buy', price=our_bid,
                                  size=size, post_only=True)
             except Exception as error:
-                print(error)
+                pass
+                # print(error)
         else:
             try:
                 if not cur_bid == our_bid:
                     rest.modify_order(cur_bid_id, price=our_bid)
             except Exception as error:
-                print(error)
+                pass
+                # print(error)
 
         # check if ask, otherwise modify
         if noask:
@@ -188,13 +195,17 @@ while True:
                 rest.place_order(market, side='sell', price=our_ask,
                                  size=size, post_only=True)
             except Exception as error:
-                print(error)
+                pass
+                # print(error)
         else:
             try:
                 if not cur_ask == our_ask:
                     rest.modify_order(cur_ask_id, price=our_ask)
             except Exception as error:
-                print(error)
-
-        print(f'time elapsed: {time.time()-start}')
-        print(f'{market} | {our_bid} | {mid} | {our_ask} | {np.round(delta, 3)}')
+                pass
+                # print(error)
+        print('-'*69)
+        print('market  |  bid  |  mid  |  ask  |  delta  |  pnl  |  time loop  ')
+        print(
+            f'{market} | {np.round(our_bid, 3)} | {np.round(mid, 3)} | {np.round(our_ask, 3)} | {np.round(delta, 3)} |{np.round(rest.get_total_usd_balance() - init_usd, 3)} | {np.round(time.time()-start, 3)}')
+        print('-'*69)
